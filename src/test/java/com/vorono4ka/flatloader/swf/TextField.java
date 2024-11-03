c7142b8e903e492febf22c485b99f4111c0d252b
Danila <crowo4ka@gmail.com>
2024-11-03 05:25:56 +0300

    chore(sc2 test): unused with default value removed

--- src/test/java/com/vorono4ka/flatloader/swf/TextField.java
+++ src/test/java/com/vorono4ka/flatloader/swf/TextField.java
@@ -1,13 +1,11 @@
 package com.vorono4ka.flatloader.swf;
 
-import com.vorono4ka.flatloader.annotations.DefaultValue;
 import com.vorono4ka.flatloader.annotations.Offset;
 import com.vorono4ka.flatloader.annotations.StructureSize;
 
 @StructureSize(40)
 public record TextField(
     @Offset(0) short id,
-    @DefaultValue short unused,
     @Offset(4) int fontNameReferenceId,
     @Offset(8) Rect bounds,
     @Offset(16) int color,
